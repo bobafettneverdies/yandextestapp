d670f264816912be11cec623e726cd1b0d7042da
Dmitrii <komiakov.dmitrii@gmail.com>
2017-04-14 21:22:46 +0500

    Gson providing in data module

--- app/src/main/java/com/blahblah/yandextestapp/di/modules/DataModule.java
+++ app/src/main/java/com/blahblah/yandextestapp/di/modules/DataModule.java
@@ -2,24 +2,24 @@ package com.blahblah.yandextestapp.di.modules;
 
 import android.support.annotation.NonNull;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import io.realm.Realm;
-
-/**
- * Created by 123 on 13.04.2017.
- */
 
 @Module
 public class DataModule {
 
-//    @Provides
-//    @Singleton
-//    @NonNull
-//    public Realm provideRealm () {
-//        return Realm.getDefaultInstance();
-//    }
+    @Provides
+    @Singleton
+    @NonNull
+    Gson provideGson() {
+        return new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
+    }
 
 }
