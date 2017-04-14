4dc3653ee56a4247fab723668efc727bf5adbe5e
Dmitrii <komiakov.dmitrii@gmail.com>
2017-04-14 21:52:20 +0500

    Translation Implements Comparable for Sorting

--- app/src/main/java/com/blahblah/yandextestapp/domain/translation/Translation.java
+++ app/src/main/java/com/blahblah/yandextestapp/domain/translation/Translation.java
@@ -1,12 +1,14 @@
 package com.blahblah.yandextestapp.domain.translation;
 
+import android.support.annotation.NonNull;
+
 import io.realm.RealmModel;
 
 /**
  * Created by 123 on 13.04.2017.
  */
 
-public class Translation implements RealmModel {
+public class Translation implements RealmModel, Comparable<Translation> {
 
     public String source;
 
@@ -18,4 +20,15 @@ public class Translation implements RealmModel {
 
     public boolean isFavorite;
 
+
+    @Override
+    public int compareTo(@NonNull Translation o) {
+        if (time > o.time) {
+            return 1;
+        } else if (time < o.time) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
