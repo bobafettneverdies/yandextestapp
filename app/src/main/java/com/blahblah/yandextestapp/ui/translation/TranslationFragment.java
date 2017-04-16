22d5a24cda19dd575a697d91a7e6329f5e9564e7
Dmitrii <komiakov.dmitrii@gmail.com>
2017-04-16 21:56:30 +0500

    TranslationFragment impl of Translationview's methods

--- app/src/main/java/com/blahblah/yandextestapp/ui/translation/TranslationFragment.java
+++ app/src/main/java/com/blahblah/yandextestapp/ui/translation/TranslationFragment.java
@@ -1,10 +1,14 @@
 package com.blahblah.yandextestapp.ui.translation;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 
 import com.blahblah.yandextestapp.R;
 import com.blahblah.yandextestapp.ui.base.BaseFragment;
+
+import javax.inject.Inject;
 
 /**
  * Created by Dmitrii Komiakov
@@ -12,7 +16,16 @@ import com.blahblah.yandextestapp.ui.base.BaseFragment;
  * komyakovds@byndyusoft.com
  * on 14.04.2017.
  */
-public class TranslationFragment extends BaseFragment  implements TranslationView {
+public class TranslationFragment extends BaseFragment implements TranslationView {
+
+    private AppCompatTextView srcLanguageView;
+    private AppCompatTextView dstLanguageView;
+    private AppCompatEditText translationInput;
+    private AppCompatTextView translationResultView;
+
+    @Inject
+    TranslationPresenter presenter;
+
     @Override
     protected int getLayoutRes() {
         return R.layout.fragment_translation;
@@ -20,26 +33,31 @@ public class TranslationFragment extends BaseFragment  implements TranslationVie
 
     @Override
     protected void onViewInflated(@NonNull View view) {
-
+        srcLanguageView = (AppCompatTextView) view.findViewById(R.id.translation_src_language);
+        dstLanguageView = (AppCompatTextView) view.findViewById(R.id.translation_dst_language);
+        translationInput = (AppCompatEditText) view.findViewById(R.id.translation_src_input);
+        translationResultView = (AppCompatTextView) view.findViewById(R.id.translation_result_view);
     }
 
     @Override
     public void setSrcLanguage(String language) {
-
+        srcLanguageView.setText(language);
     }
 
     @Override
     public void setDstLanguage(String language) {
-
+        dstLanguageView.setText(language);
     }
 
     @Override
     public void swapLanguages() {
-
+        String dstLanguage = dstLanguageView.getText().toString();
+        dstLanguageView.setText(srcLanguageView.getText());
+        srcLanguageView.setText(dstLanguage);
     }
 
     @Override
     public void setTranslation(String translation) {
-
+        translationResultView.setText(translation);
     }
 }
